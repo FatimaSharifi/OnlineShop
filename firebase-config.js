88dbb5e34b4246ed21b3566faac54866db971f9d
FATIMA SHARIFI <157017888+FatimaSharifi@users.noreply.github.com>
2026-07-07 17:09:54 +0600

    Remove storageBucket from firebase-config.js
    Removed storageBucket from Firebase configuration.

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,6 @@ const firebaseConfig = {
   apiKey: "AIzaSyAb-36QLk_dOgnRKNTYai3W22O1Avf6Bn0",
   authDomain: "javedanshop.firebaseapp.com",
   projectId: "javedanshop",
-  storageBucket: "javedanshop.firebasestorage.app",
   messagingSenderId: "811886915801",
   appId: "1:811886915801:web:9ee1ca9cba6b66d5bb0ec8",
   measurementId: "G-BBHL134YTD"
